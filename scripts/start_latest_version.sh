457869c9be67a893b79a021f431ee41cf9abefbe
ansonallard <ansononlineinfo@gmail.com>
2026-05-23 20:44:37 -0400

    fix: Use relative path for build

--- scripts/start_latest_version.sh
+++ scripts/start_latest_version.sh
@@ -6,6 +6,5 @@ SERVICE_NAME="deployment-service"
 git fetch
 tag=$(git --no-pager tag --sort=-creatordate | head -1)
 git checkout $tag
-MAIN_DIR=$(find "$PWD" -type d -path "*/cmd/$SERVICE_NAME")
-go build -o $SERVICE_NAME $MAIN_DIR
+go build -o $SERVICE_NAME "./cmd/$SERVICE_NAME"
 sudo systemctl restart deployment-service
