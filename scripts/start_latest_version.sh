0d7f5740c9ac6ae11fa0a4c492a699f54336aebe
ansonallard <ansononlineinfo@gmail.com>
2026-05-23 20:51:31 -0400

    chore: Remove quotes from script

--- scripts/start_latest_version.sh
+++ scripts/start_latest_version.sh
@@ -6,5 +6,5 @@ SERVICE_NAME="deployment-service"
 git fetch
 tag=$(git --no-pager tag --sort=-creatordate | head -1)
 git checkout $tag
-go build -o $SERVICE_NAME "./cmd/$SERVICE_NAME"
+go build -o $SERVICE_NAME ./cmd/$SERVICE_NAME
 sudo systemctl restart deployment-service
